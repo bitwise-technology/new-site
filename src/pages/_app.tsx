7fad1f62721fa3f8525b1ee4a1f3fde22862bee6
Tiago Jose <tiago_jvo@outlook.com>
2021-03-07 18:19:47 -0300

    feat: add new global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
-import GlobalStyles from 'themes/global'
+import { GlobalStyles } from 'themes/global'
 
 function App({ Component, pageProps }: AppProps) {
   return (
