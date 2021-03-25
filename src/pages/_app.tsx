039dfe015c6b3f6bb9fe2a8af98ff59fa65f35d7
Tiago Jose <tiago_jvo@outlook.com>
2021-03-25 10:20:04 -0300

    feat: changes in website title and icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,11 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Next.js Pro - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon.png" />
-        <link rel="apple-touch-icon" href="/img/icon.png" />
+        <title>Bitwise - 2021</title>
+        <link rel="shortcut icon" href="/img/Logo.svg" />
+        <link rel="apple-touch-icon" href="/img/Logo.svg" />
         <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple Next.js Boilerplate Ready to production"
-        />
+        <meta name="description" content="Novo site da bitwise technology" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
