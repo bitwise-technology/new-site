d3a3208429e128e7622b7976d62c3ca0d0e4c55d
Tiago Jose <tiago_jvo@outlook.com>
2021-03-25 10:32:30 -0300

    refactor: changes in RadioInput component styles

--- src/ui/components/RadioInput/RadioInputStyles.ts
+++ src/ui/components/RadioInput/RadioInputStyles.ts
@@ -20,20 +20,29 @@ export const RadioInputLabel = styled.label`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 2rem;
+  margin-top: 1rem;
   font-size: 1.4rem;
   line-height: 2rem;
   color: #5a3d5c;
   cursor: pointer;
-
   &::before {
     content: '';
     display: inline-block;
-    width: 1rem;
-    height: 1rem;
+    width: 1.2rem;
+    height: 1.2rem;
     border: 2px solid #5a3d5c;
     background-color: none;
     border-radius: 50%;
     margin-right: 1rem;
+  }
+  @media screen and (min-width: 768px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 2rem;
+    font-size: 1.4rem;
+    line-height: 2rem;
+    color: #5a3d5c;
+    cursor: pointer;
   }
 `
