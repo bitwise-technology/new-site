281497cb3de74e59b22cc86bad82991d0d6c3afe
Tiago Jose <tiago_jvo@outlook.com>
2021-03-25 10:31:51 -0300

    refactor: changes in ProgrammingLanguage component styles and add alt to images

--- src/ui/components/ProgrammingLanguages/ProgrammingLanguagesStyles.ts
+++ src/ui/components/ProgrammingLanguages/ProgrammingLanguagesStyles.ts
@@ -22,59 +22,59 @@ export const ProgrammingLanguagesTitle = styled.h3`
 `
 
 export const Languages = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
+  grid-gap: 5rem;
+
+  @media screen and (min-width: 1400px) {
+    grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
+  }
+
+  @media screen and (min-width: 1800px) {
+    grid-template-columns: repeat(auto-fill, minmax(40rem, 1fr));
+  }
 `
 
 export const LanguageBox = styled.div`
   background: #fff;
   display: flex;
-  flex-direction: row;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 
   padding-bottom: 3rem;
 
   box-shadow: 2px 5px 5px #bbb;
 
+  text-align: center;
+
   border-radius: 1rem;
 
   font-family: 'Gotham';
-  width: 100%;
 
   img {
     margin-top: 2rem;
-    align-self: flex-start;
-    max-width: 150px;
-    max-height: 150px;
-  }
+    max-width: 50px;
+    max-height: 50px;
 
-  @media screen and (max-width: 738px) {
-    &:not(:last-child) {
-      margin-bottom: 3rem;
+    @media screen and (min-width: 1400px) {
+      align-self: flex-start;
     }
   }
-  @media screen and (min-width: 738px) {
-    margin: 3rem 0;
-
-    width: 49%;
-  }
-  @media screen and (min-width: 992px) {
-    width: 31%;
-    &::not(:nth-child(3n)) {
-      margin-right: 5rem;
-    }
+  @media screen and (min-width: 1400px) {
+    flex-direction: row;
+    justify-content: center;
+    text-align: left;
   }
 `
 
 export const LanguageName = styled.p`
   font-family: 'Gotham-Bold';
   font-size: 1.4rem;
-  text-transform: uppercase;
   margin-top: 1.8rem;
 
-  @media screen and (min-width: 992px) {
+  @media screen and (min-width: 1400px) {
+    text-transform: uppercase;
+
     font-size: 1.6rem;
   }
 `
@@ -84,11 +84,18 @@ export const LanguageText = styled.div`
   line-height: 2rem;
   max-width: 24rem;
   margin-top: 0.5rem;
-  padding-bottom: 2rem;
   color: #000;
-  margin-left: 3rem;
+  @media screen and (min-width: 1400px) {
+    font-size: 1.2rem;
+    line-height: 2rem;
+    max-width: 24rem;
+    margin-top: 0.5rem;
+    padding-bottom: 2rem;
+    color: #000;
+    margin-left: 3rem;
+  }
 
-  @media screen and (min-width: 992px) {
+  @media screen and (min-width: 1400px) {
     font-size: 1.3em;
   }
 `
--- src/ui/components/ProgrammingLanguages/index.tsx
+++ src/ui/components/ProgrammingLanguages/index.tsx
@@ -16,55 +16,64 @@ import {
 const ProgrammingLanguages = () => {
   const { selectedLanguage } = useContext(LanguagesContext)
 
+  const programmingLanguagesStrings =
+    strings[selectedLanguage].programming_languages
+
   const languages = [
     {
       name: 'Rust',
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-      image: '/img/languages/Rust_programming_language_black_logo.svg'
+      image: '/img/languages/Rust_programming_language_black_logo.svg',
+      alt: programmingLanguagesStrings.first_language.alt
     },
     {
       name: 'Typescript',
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-      image: '/img/languages/Typescript_logo_2020.svg'
+      image: '/img/languages/Typescript_logo_2020.svg',
+      alt: programmingLanguagesStrings.second_language.alt
     },
     {
       name: 'Golang',
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-      image: '/img/languages/Go_Logo_Blue.svg'
+      image: '/img/languages/Go_Logo_Blue.svg',
+      alt: programmingLanguagesStrings.third_language.alt
     },
     {
       name: 'Node.JS',
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-      image: '/img/languages/Node.js_logo.svg'
+      image: '/img/languages/Node.js_logo.svg',
+      alt: programmingLanguagesStrings.fourth_language.alt
     },
     {
       name: 'Blockchain',
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-      image: '/img/languages/blockchain-logo.svg'
+      image: '/img/languages/blockchain-logo.svg',
+      alt: programmingLanguagesStrings.fifth_language.alt
     },
     {
       name: 'Fintechs',
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-      image: '/img/languages/credit card alt.svg'
+      image: '/img/languages/credit card alt.svg',
+      alt: programmingLanguagesStrings.sixth_language.alt
     }
   ]
 
   return (
     <ProgrammingLanguagesContainer>
       <ProgrammingLanguagesTitle>
-        {strings[selectedLanguage].programming_languages.title}
+        {programmingLanguagesStrings.title}
       </ProgrammingLanguagesTitle>
       <Languages data-testid="languages">
-        {languages.map(({ name, description, image }) => {
+        {languages.map(({ name, description, image, alt }) => {
           return (
             <LanguageBox key={name}>
-              <img src={image} />
+              <img src={image} alt={alt} />
               <LanguageText>
                 <LanguageName>{name}</LanguageName>
                 <LanguageDescription>{description}</LanguageDescription>
