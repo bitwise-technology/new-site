bfcecf3f43358890618e06f9371382281d437550
Lucas Erthal <57104379+LucasErthal@users.noreply.github.com>
2022-03-09 10:37:39 -0300

    feat: section1 image changed for png and using next/image

--- src/ui/components/Section1/Section1Styles.ts
+++ src/ui/components/Section1/Section1Styles.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components'
 
 export const Container = styled.section`
-  background-image: url("/img/Section1-Background.svg");
+  position: relative;
   background-color: #534554;
   background-repeat: no-repeat;
   background-size: cover;
 `
 
 export const Shader = styled.div`
+  position: relative;
   background-color: rgba(0, 0, 0, 0.6)
 `;
--- src/ui/components/Section1/index.tsx
+++ src/ui/components/Section1/index.tsx
@@ -1,10 +1,21 @@
 import Motto from '../Motto'
 import Header from '../Header'
 import { Container, Shader } from './Section1Styles'
+import Image, { ImageLoaderProps } from 'next/image'
 
 const Section1 = () => {
+  const myLoader = ({ src, quality }:ImageLoaderProps) => {
+  return `${src}?q=${quality || 100}`
+}
+
   return (
     <Container>
+      <Image
+      loader={myLoader}
+      src="./img/Section1-Background2.png"
+      layout="fill"
+      priority
+    />
       <Shader>
         <Header />
         <Motto />
